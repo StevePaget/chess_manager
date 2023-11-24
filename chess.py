5d0934edaea4980249478aa90c94f08d45165fd8
Steve Paget <StevePaget@users.noreply.github.com>
2023-11-24 14:07:58 +0000

    Update chess.py
    Added some comments

--- chess.py
+++ chess.py
@@ -21,17 +21,19 @@ class App(tk.Tk):
         self.db = sqlite3.connect("chess.db")
 
 
-        self.frames= [PlayerEdit(self) ]
+        self.frames= [PlayerEdit(self) ] # Make instances of all the different screens, and store them in a list
 
-        self.demoData()
+        self.demoData() # this creates a single user. This would normally be commented out
         self.switchScreen(0)
         self.mainloop()
 
     def switchScreen(self,screen):
-        self.frames[screen].grid(row=2,column=0,sticky="NSEW")
-        self.frames[screen].load()
+        for s in self.frames:  # go through each screen and make it forget its grid position, effectively hiding it
+            s.grid_forget()
+        self.frames[screen].grid(row=2,column=0,sticky="NSEW") # this puts the new screen on the grid, so it is visible
+        self.frames[screen].load()  # because the new screen might need to refresh its data, they all need a load method
 
-    def demoData(self):
+    def demoData(self):  # this just deletes old data and creates a person, for test purposes. This would not nornally be run
             c = self.db.cursor()
             c.execute("DELETE from Players")
             c.execute("INSERT INTO Players VALUES (NULL, 'Sally', 'Smith', '07KFR')")
